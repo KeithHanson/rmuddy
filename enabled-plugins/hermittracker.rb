da7c56b224c84411e470ba6cad77104c12c7ecb8
Demonnic Bloodbather <demonnic@honeywine.(none)>
2008-03-25 20:40:01 -0400

    First version of Hermittracker This should, if the notify support is functioning, help you to keep track of which hermit cards go where. It uses a hash database, and save the information to a yaml document for use between sessions.

---
+++ enabled-plugins/hermittracker.rb
@@ -0,0 +1,60 @@
+#Module for tracking where you have charged Hermit tarot cards in achaea
+#USAGE: /notify 4567 Hermittracker activate_hermit <word to associate room with>
+#       /notify 4567 Hermittracker fling_hermit <word you tagged the room you want to go to with>
+#The tracker -should- automagically keep track of which card you charged, and 
+#where, and get it from your pack.
+module Hermittracker
+  def hermittracker_setup
+    @whichhermit = ''
+    @key = ''
+    @charginghermit = false
+    warn("Loading hermit locations database")
+    @hermithash = YAML.load(File.open("configs/hermithash.yaml"))
+    trigger /card(\d+)\s+a tarot card inscribed with the Hermit/, :set_value
+    trigger /You take the Hermit tarot and rub it vigorously on the ground/, :save_hash
+    trigger /^The card begins to glow with a mystic energy/, :hermit_drop
+    send_kmuddy_command("ind 50 hermit")
+    warn("Hermit Tracker loaded")
+  end
+  
+  def activate_hermit(key)
+    if key == ''
+      warn("You must supply a word to associate this room with!!")
+    else
+      warn("Ok, activating a hermit for this room")
+      send_kmuddy_command("outd hermit")
+      send_kmuddy_command("ii hermit")
+      @key = key
+      warn("Associating card @whichhermit with the place name @key")
+      @hermithash[key] = @whichhermit
+      send_kmuddy_command("activate hermit")
+    end
+  end
+  
+  def set_value(match_object )
+    @whichhermit = match_object[1]
+  end
+  
+  def save_hash
+    File.open("configs/hermithash.yaml", "w") {|f| YAML.dump(@hermithash, f)}
+    warn("Saved hermit tracker hash")
+  end
+  
+  def fling_hermit(key)
+    if key == ''
+      warn("Come now, you have to tell me where to go! Specify a hermit to fling!")
+    else
+      send_kmuddy_command("get @hermithash[key] from pack")
+      send_kmuddy_command("charge hermit")
+      @charginghermit = true
+    end
+  end
+  
+  def hermit_drop
+    if @charginghermit
+      send_kmuddy_command("fling hermit at ground")
+      @charginghermit = false
+      @hermithash.delete("@key")
+    end
+  end
+end
