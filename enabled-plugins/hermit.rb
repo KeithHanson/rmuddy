0f7d427f4ca0bcef20a532c2c1ee04479f02efc3
Demonnic Bloodbather <demonnic@honeywine.(none)>
2008-03-27 20:47:14 -0400

    Added formatting to hermit_list in Hermit plugin. Added del_hash and reset_hash methods to Hermit plugin.
    del_hash(key) will remove a single key from the hash storing your hermit cards. You would use this is one of your cards had decayed to dust... I will add support for this to be automagic at some point, but for now, it's manual.
    
    reset_hash()  will reset your entire hash back to blanks (except for the place holder "Location" => "card number" key=>value pair) in case you've been gone for a couple of days and all of your activated hermit cards have turned to dust... again, this will be automagical at some point, for now be glad I thought of it at all =P~

--- enabled-plugins/hermit.rb
+++ enabled-plugins/hermit.rb
@@ -8,23 +8,26 @@ class Hermit < BasePlugin
   def setup
     @whichhermit = ''
     @key = ''
+    @formatlength = 80
+    @formatpad = (@formatlength / 3)
     @charginghermit = false
-    @resethash = {"Location" => "placeholder"}
-    warn("Loading hermit locations database")
+    @resethash = {"Location" => "card number"}
+    warn("RMuddy: Loading hermit locations database")
     File.open("configs/hermithash.yaml") {|fi| @hermithash = YAML.load(fi)}
+    trigger /^You have recovered equilibrium.$/, :put_away_hermit
     trigger /card(\d+)\s+a tarot card inscribed with the Hermit/, :set_value
     trigger /You take the Hermit tarot and rub it vigorously on the ground/, :save_hash
     trigger /^The card begins to glow with a mystic energy/, :hermit_drop
     send_kmuddy_command("ind 50 hermit")
-    warn("Hermit Tracker loaded")
+    warn("RMuddy: Hermit Tracker loaded")
   end
   
   def activate_hermit(key = '')
     if key == ''
-      warn("Rmuddy: You must supply a word to associate this room with!!")
+      warn("RMuddy: You must supply a word to associate this room with!!")
     else
       @key = key
-      warn("Rmuddy: Ok, activating a hermit for this room")
+      warn("RMuddy: Ok, activating a hermit for this room")
       send_kmuddy_command("outd hermit")
       send_kmuddy_command("ii hermit")
     end
@@ -37,20 +40,21 @@ class Hermit < BasePlugin
   end
   
   def associate_hermit
-      warn("Rmuddy: Associating card #{@whichhermit} with the place name #{@key}")
+      warn("RMuddy: Associating card #{@whichhermit} with the place name #{@key}")
       @hermithash[@key] = @whichhermit
       send_kmuddy_command("activate hermit")
+      @activatinghermit = true
   end
   
   def save_hash
     File.open("configs/hermithash.yaml", "w") {|fi| YAML.dump(@hermithash, fi)}
-    warn("Saved hermit tracker hash")
+    warn("RMuddy: Saved hermit tracker hash")
   end
   
   def fling_hermit(key = '')
     @key = key
     if key == ''
-      warn("Come now, you have to tell me where to go! Specify a hermit to fling!")
+      warn("RMuddy: Come now, you have to tell me where to go! Specify a hermit to fling!")
     else
       send_kmuddy_command("get #{@hermithash[key]} from pack")
       send_kmuddy_command("charge hermit")
@@ -58,13 +62,36 @@ class Hermit < BasePlugin
     end
   end
   
+  def put_away_hermit
+    if @activatinghermit
+      send_kmuddy_command("Put hermit in pack")
+      @activatinghermit = false
+    end
+  end
+  
+  def del_hash (key = '')
+    if key == ''
+      warn("RMuddy: You must specify the room tag you wish to remove from the database")
+    else
+      warn("RMuddy deleting #{key} from the hermit locations database")
+      @hermithash.delete(key.to_s)
+    end
+  end
+  
   def hermit_list
-    warn("Rmuddy: Hermits currently in database")
-    @hermithash.each_key { |key| warn("Rmuddy: #{key}") }
+    warn("RMuddy: Hermits currently in database")
+    @output = "Rmuddy: Location".ljust(@formatpad) + @hermithash["Location"].rjust(@formatpad)
+    warn(@output)
+    @hermithash.each_key { |key| 
+     unless key == "Location" 
+       @output = "Rmuddy: #{key.to_s}".ljust(@formatpad) + @hermithash[key].to_s.rjust(@formatpad)
+       warn(@output)
+     end
+    }
   end
   
   def reset_hash
-    warn("Rmuddy: Resetting hermit database... all hermit location is now kaput")
+    warn("RMuddy: Resetting hermit database... all hermit location is now kaput")
     @hermithash = @resethash
     save_hash
   end
