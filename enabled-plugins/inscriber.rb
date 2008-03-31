16a9ee46966d40f8127040133f230f70e1454ea3
Demonnic Bloodbather <demonnic@honeywine.(none)>
2008-03-30 22:26:37 -0400

    Added pause_inscription and unpause_inscription methods for use pausing and unpausing batch jobs. It will cause it to pause the next time it checks if you should inscribe using simple boolean logic.

--- enabled-plugins/inscriber.rb
+++ enabled-plugins/inscriber.rb
@@ -16,6 +16,8 @@ attr_accessor :inscribing
     trigger /^You lack the mental resources to etch a Tarot card./, :out_of_mana! #oops, ran out of mana
     
     #by default, we are not, in fact, inscribing
+    @paused = false
+    @batch_total = 0
     @inscribing = false
     @number_to_inscribe = 0
     @type_to_inscribe = ""
@@ -24,6 +26,7 @@ attr_accessor :inscribing
   def mass_inscribe(number_to_inscribe, type_to_inscribe)
     @number_to_inscribe = number_to_inscribe.to_i
     @type_to_inscribe = type_to_inscribe.to_s
+    @batch_total = number_to_inscribe.to_i
     set_kmuddy_variable("number_to_inscribe", @number_to_inscribe)
     set_kmuddy_variable("type_to_inscribe", @type_to_inscribe)
     warn("Inscriber Plugin: Ready to inscribe #{@number_to_inscribe} #{@type_to_inscribe}")
@@ -49,15 +52,25 @@ attr_accessor :inscribing
   
   def out_of_mana!  #pretty obvious
     @inscribing = false  #if we're out of mana, we're not inscribing
+    plugins[Sipper].enable
     send_kmuddy_command("sip mana")  #so we need to sip some mana
   end
   
+  def pause_inscription
+    @paused = true
+  end
+  
+  def unpause_inscription
+    @paused = false
+    inscribe_tarot
+  end
+  
   def should_we_inscribe?  #test if we should inscribe
-    if @number_to_inscribe > 0  #if we still have inscription to do
+    if @number_to_inscribe > 0  && !@paused #if we still have inscription to do and we're not paused
       inscribe_tarot   #then inscribe
     elsif @number_to_inscribe == 0  #otherwise, if there are 0 left to do
       disabled_plugins[Sipper].enable unless disabled_plugins[Sipper].nil?
-      send_kmuddy_command("ind 50 #{@type_to_inscribe}")  #put the cards away
+      send_kmuddy_command("ind #{@batch_total} #{@type_to_inscribe}")  #put the cards away
       plugins[Sipper].should_i_sip? #check if we need to sip 
     end
   end
